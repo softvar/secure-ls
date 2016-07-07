463ce77bd7d0b98b2ad099e9eacd2fb814e48c5b
softvar <varun2902@gmail.com>
2016-07-08 00:59:38 +0530

    add enc-utf8 file, dependency for cryptojs encryption

---
+++ src/enc-utf8.js
@@ -0,0 +1,30 @@
+let enc = {};
+
+enc.Latin1 = {
+  stringify: (wordArray) => {
+    // Shortcuts
+    let words = wordArray.words;
+    let sigBytes = wordArray.sigBytes;
+    let latin1Chars = [], i, bite;
+
+    // Convert
+    for (i = 0; i < sigBytes; i++) {
+      bite = (words[i >>> 2] >>> (24 - (i % 4) * 8)) & 0xff;
+      latin1Chars.push(String.fromCharCode(bite));
+    }
+
+    return latin1Chars.join('');
+  }
+};
+
+enc._Utf8 = {
+  stringify: (wordArray) => {
+    try {
+      return decodeURIComponent(escape(enc.Latin1.stringify(wordArray)));
+    } catch (e) {
+      throw new Error('Malformed UTF-8 data');
+    }
+  }
+};
+
+module.exports = enc;
