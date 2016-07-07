52a4d745b338154e4bff55b20edea8728eb14a61
softvar <varun2902@gmail.com>
2016-07-08 01:11:03 +0530

    only base64 encoding script example

---
+++ example/only-base64.js
@@ -0,0 +1,10 @@
+var key = 'only__base64';
+var data = {data: [{age: 1}, {age: '2'}]};
+var o_b = new library({isCompression: false});
+ae = o_b.AES.encrypt(JSON.stringify(data), 's3cr3t@123');
+bde = o_b.AES.decrypt(ae.toString(), 's3cr3t@123');
+de = bde.toString(o_b.enc._Utf8);
+
+o_b.set(key, data);
+console.log(o_b.get(key));
+console.log('____________________________________')
