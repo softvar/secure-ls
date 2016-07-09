84db1dff8796fffd898125d61c551d27e11f71fb
softvar <varun2902@gmail.com>
2016-07-10 00:16:55 +0530

    added basic examples

---
+++ example/base64-compressed.js
@@ -0,0 +1,10 @@
+var key = 'base64__compressed';
+var data = {data: [{age: 1}, {age: '2'}]};
+var b_c = new library();
+ae = b_c.AES.encrypt(JSON.stringify(data), 's3cr3t@123');
+bde = b_c.AES.decrypt(ae.toString(), 's3cr3t@123');
+de = bde.toString(b_c.enc._Utf8);
+
+b_c.set(key, data);
+console.log(b_c.get(key));
+console.log('____________________________________')
---
+++ example/only-compressed.js
@@ -0,0 +1,10 @@
+var key = 'only__compressed';
+var data = {data: [{age: 1}, {age: '2'}]};
+var o_c = new library({encodingType: ''});
+ae = o_c.AES.encrypt(JSON.stringify(data), 's3cr3t@123');
+bde = o_c.AES.decrypt(ae.toString(), 's3cr3t@123');
+de = bde.toString(o_c.enc._Utf8);
+
+o_c.set(key, data);
+console.log(o_c.get(key));
+console.log('____________________________________')
