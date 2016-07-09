e85a100b4a304bab0c599d3bd1135ce65fafa955
softvar <varun2902@gmail.com>
2016-07-10 00:17:12 +0530

    added aes + compress/uncompress example

---
+++ example/aes-compressed.js
@@ -0,0 +1,10 @@
+var key = 'aes__compressed';
+var data = {data: [{age: 1}, {age: '2'}]};
+var aes_c = new library({encodingType: 'aes'});
+ae = aes_c.AES.encrypt(JSON.stringify(data), 's3cr3t@123')
+bde = aes_c.AES.decrypt(ae.toString(), 's3cr3t@123')
+de = bde.toString(aes_c.enc._Utf8)
+
+aes_c.set(key, data);
+console.log(aes_c.get(key));
+console.log('____________________________________')
