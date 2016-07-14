a2d68b7ad815c2b7d5b8d2b46e15aedc99973353
softvar <varun2902@gmail.com>
2016-07-15 01:47:31 +0530

    use of encryptionSecret key

--- example/aes-compressed.js
+++ example/aes-compressed.js
@@ -1,8 +1,8 @@
 var key = 'aes__compressed';
 var data = {data: [{age: 1}, {age: '2'}]};
-var aes_c = new SecureLS({encodingType: 'aes'});
-ae = aes_c.AES.encrypt(JSON.stringify(data), 's3cr3t@123')
-bde = aes_c.AES.decrypt(ae.toString(), 's3cr3t@123')
+var aes_c = new SecureLS({encodingType: 'aes', encryptionSecret: ''});
+ae = aes_c.AES.encrypt(JSON.stringify(data), '')
+bde = aes_c.AES.decrypt(ae.toString(), '')
 de = bde.toString(aes_c.enc._Utf8)
 
 aes_c.set(key, data);
