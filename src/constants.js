4e10414a7b7bd13357bc8544034e5ccb2e16ad4b
softvar <varun2902@gmail.com>
2016-07-15 01:50:25 +0530

    update missing enum value

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,7 @@
 let WarningEnum = {
   KEY_NOT_PROVIDED: 'keyNotProvided',
-  META_KEY_REMOVE: 'metaKeyRemove'
+  META_KEY_REMOVE: 'metaKeyRemove',
+  DEFAULT_TEXT: 'defaultText'
 };
 
 let WarningTypes = {};
@@ -8,9 +9,9 @@ let WarningTypes = {};
 WarningTypes[WarningEnum.KEY_NOT_PROVIDED] = 'Secure LS: Key not provided. Aborting operation!';
 WarningTypes[WarningEnum.META_KEY_REMOVE] = `Secure LS: Meta key can not be removed
 unless all keys created by Secure LS are removed!`;
+WarningTypes[WarningEnum.DEFAULT_TEXT] = `Unexpected output`;
 
 let constants = {
-  DEFAULT_WARNING_TEXT: 'Unexpected output',
   WarningEnum: WarningEnum,
   WarningTypes: WarningTypes,
   EncrytionTypes: {
