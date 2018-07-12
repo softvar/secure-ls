7f7cf919737d90fbf08df4bda2db23c33e9293c1
Paul Csapak <office@webcascade.com>
2018-07-12 16:13:54 +0200

    add additional tests for new methods

--- test/localStorage.spec.js
+++ test/localStorage.spec.js
@@ -123,4 +123,49 @@ describe('LocalSorage API Tests ->', () => {
       expect(value2).to.not.be.an('array');
     });
   });
+
+  describe('execute localStorage/sessionStorage style methods', () => {
+    it('should use setItem, getItem and removeItem', () => {
+      let value1, value2;
+      let data = [1, 2, 3];
+      let key1 = 'key-1', key2 = 'key-2';
+
+      lib.setItem(key1, data);
+      expect(mockLS.storage[key1]).to.exist;
+      expect(mockLS.storage[key1]).to.be.a('string');
+
+      lib.setItem(key2, data);
+      expect(mockLS.storage[key2]).to.exist;
+      expect(mockLS.storage[key2]).to.be.a('string');
+
+      value1 = lib.getItem(key1);
+      expect(value1).to.be.an('array');
+      expect(value1.length).to.equal(3);
+      expect(value1.toString()).to.equal(data.toString());
+
+      value2 = lib.getItem(key2);
+      expect(value2).to.be.an('array');
+      expect(value2.length).to.equal(3);
+      expect(value2.toString()).to.equal(data.toString());
+
+      lib.remove(key1);
+      value1 = lib.getItem(key1);
+      expect(mockLS.storage[key1]).to.not.exist;
+      expect(value1).to.not.be.an('array');
+
+      value2 = lib.getItem(key2);
+      expect(value2).to.be.an('array');
+      expect(value2.length).to.equal(3);
+      expect(value2.toString()).to.equal(data.toString());
+
+      lib.remove(key2);
+      value1 = lib.getItem(key1);
+      expect(mockLS.storage[key1]).to.not.exist;
+      expect(value1).to.not.be.an('array');
+
+      value2 = lib.getItem(key2);
+      expect(mockLS.storage[key2]).to.not.exist;
+      expect(value2).to.not.be.an('array');
+    });
+  });
 });
