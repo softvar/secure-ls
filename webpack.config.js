610d4f0d3b771061a8fec3d446a4f3e38d759284
softvar <varun2902@gmail.com>
2024-06-20 03:51:48 +0530

    chore: add configuration files for dev environmentl update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,103 @@
-var webpack = require('webpack');
-var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
-var path = require('path');
-var env = require('yargs').argv.mode;
+const webpack = require('webpack');
+const path = require('path');
+const semver = require('semver');
 
-var libraryName = 'secure-ls';
+const packageFile = require('./package.json');
+const libVersion = packageFile.version;
+const libraryName = packageFile.name;
 
-var plugins = [], outputFile;
+const PRODUCTION = 'production';
 
-if (env === 'build') {
-  plugins.push(new UglifyJsPlugin({ minimize: true }));
-  outputFile = libraryName + '.min.js';
-} else {
-  outputFile = libraryName + '.js';
+let deps = '';
+
+Object.keys(packageFile.dependencies).map((key, index) => {
+  deps += `\n ${index + 1}. ${key} - ${packageFile.dependencies[key]}`;
+});
+
+let libraryHeaderComment;
+
+function addPlugins(argv) {
+  const version = semver.inc(libVersion, argv.env.type) || libVersion;
+
+  libraryHeaderComment = `${libraryName} - v${version}
+URL - https://github.com/softvar/secure-ls
+
+The MIT License (MIT)
+
+Copyright (c) 2016-2024 Varun Malhotra
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+
+Dependencies used - ${deps}`;
+
+  const plugins = [
+    new webpack.BannerPlugin({
+      banner: libraryHeaderComment,
+      entryOnly: true,
+      stage: webpack.Compilation.PROCESS_ASSETS_STAGE_REPORT,
+    }),
+  ];
+
+  return plugins;
 }
 
-var config = {
-  entry: __dirname + '/src/index.js',
-  devtool: 'source-map',
-  output: {
-    path: __dirname + '/dist',
-    filename: outputFile,
-    library: 'SecureLS',
-    libraryTarget: 'umd',
-    umdNamedDefine: true
-  },
-  module: {
-    loaders: [
-      {
-        test: /(\.jsx|\.js)$/,
-        loader: 'babel',
-        exclude: /(node_modules|bower_components)/
-      },
-      {
-        test: /(\.jsx|\.js)$/,
-        loader: "eslint-loader",
-        exclude: /node_modules/
-      }
-    ]
-  },
-  eslint: {
-    failOnWarning: false,
-    failOnError: false
-  },
-  resolve: {
-    root: path.resolve('./src'),
-    extensions: ['', '.js']
-  },
-  plugins: plugins
-};
+module.exports = function (_env, argv) {
+  return {
+    entry: {
+      [libraryName]: '/src/index.js',
+    },
+    devtool: 'source-map',
+    mode: argv.mode === PRODUCTION ? 'production' : 'development',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: () => {
+        if (argv.mode === PRODUCTION) {
+          return '[name].min.js';
+        }
 
-module.exports = config;
+        return '[name].js';
+      },
+      library: 'SecureLS',
+      libraryTarget: 'umd',
+      globalObject: 'this',
+      auxiliaryComment: {
+        root: ' Root',
+        commonjs: ' CommonJS',
+        commonjs2: ' CommonJS2',
+        amd: ' AMD',
+      },
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+          },
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.js'],
+    },
+    plugins: addPlugins(argv),
+  };
+};
